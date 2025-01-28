ae06d120c332752d752cf55e6cf666791593d5fc
YakovenkoA <aleksandrakovenko21@gmail.com>
2025-01-28 17:17:57 +0500

    cyc2801

--- циклы.py
+++ циклы.py
@@ -1,26 +1,29 @@
-"""
-x = 0
 
-while x <5:
-    x+=1
-    
-else:
-    print(x) 
-    
-    """
-    
-x = ''
+for i in range(1, 6, 2):
+  print(i)
 
-while len(x) < 5:
-  y = input ('Ввод данных: ')
-  if y == 'o':
-   continue
-
-if y == 'l':
-   break
 
   
-  x += y
+word = "Hello, world!!!"
+for i in word:
+  print(i)
   
-else
-print(x)
+  word = "Hello, world!!!"
+for i in word:
+  print(i *3)
+  
+    
+word = "Hello, world!!!"
+for i in word:
+  if i =="e":
+    print("Yes")
+
+
+
+count = 0
+word = "Hello, world!!!"
+for i in word:
+  if i == "w":
+    count += 1
+    
+  print("count", count)
