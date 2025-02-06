e75f06e7a5b6dddecf44deeaa91f57399766f9e9
YakovenkoA <aleksandrakovenko21@gmail.com>
2025-02-06 17:21:52 +0500

    commit06021

--- циклы.py
+++ циклы.py
@@ -1,4 +1,5 @@
-
+# FOR
+"""
 for i in range(1, 6, 2):
   print(i)
 
@@ -28,3 +29,53 @@ for i in word:
     
   print("count", count)
   
+"""
+
+#   WHILT
+
+"""
+i = 5
+while i <=15:
+  print(i)
+  i += 2
+  
+  
+isHasCar = True
+
+while isHasCar:
+  if input("Enter data: ") == "Stop":
+    isHasCar = False
+"""
+
+
+for i in range(1, 11):
+  print(i)
+  
+  
+for i in range(1, 11):
+  if i == 5:
+    break # Заканчивает выполнение
+  print(i)  
+  
+  
+  
+for i in range(1, 11):
+  if i == 5:
+    break
+  
+  if i % 2 ==0:
+    continue # Пропускает итерацию
+  
+  print(i)    
+
+  
+found = None
+for i in "hello":
+  if i == "l":
+    found = True
+    break
+    
+else:
+  founl = False
+  
+print(found)
